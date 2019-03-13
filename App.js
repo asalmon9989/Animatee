8830a23338ffb78502277abb7b2d49f3f02ca0fc
Andy <asalmon9989@gmail.com>
2019-03-13 15:58:33 -0700

    Finished exercise

--- App.js
+++ App.js
@@ -31,7 +31,19 @@ class App extends React.Component {
       inputRange: [0, HEADER_SCROLL_DISTANCE],
       outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
       extrapolate: 'clamp',
-    })
+    });
+
+    const imageOpacity = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE/2, HEADER_SCROLL_DISTANCE],
+      outputRange: [1, 1, 0],
+      extrapolate: 'clamp',
+    });
+    const imageTranslate = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [0, -50],
+      extrapolate: 'clamp',
+    });
+
 
     return (
       <View style={styles.fill}>
@@ -43,6 +55,13 @@ class App extends React.Component {
           {this._renderScrollViewContent()}
         </ScrollView>
         <Animated.View style={[styles.header, { height: headerHeight }]}> 
+          <Animated.Image
+            style={[
+              styles.backgroundImage,
+              {opacity: imageOpacity, transform: [{translateY: imageTranslate}]},
+            ]}
+            source={require('./images/cat.jpg')}
+          />
           <View style={styles.bar}>
             <Text style={styles.title}>Title</Text>
           </View>
@@ -85,6 +104,15 @@ const styles = StyleSheet.create({
   },
   scrollViewContent: {
     marginTop: HEADER_MAX_HEIGHT,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: null,
+    height: HEADER_MAX_HEIGHT,
+    resizeMode: 'cover',
   },
 });
 
